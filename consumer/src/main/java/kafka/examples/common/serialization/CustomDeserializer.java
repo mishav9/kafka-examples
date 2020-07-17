500cc615c02fac9ec266e21949f2d2c398cbba84
avvin <avvin@hotmail.com>
2020-07-18 00:23:11 +0530

    Edit 1 Deleted CustomDeserializer and runAdvancedConsumer.sh modified CustomDeserializer

--- consumer/src/main/java/kafka/examples/common/serialization/CustomDeserializer.java
+++ consumer/src/main/java/kafka/examples/common/serialization/CustomDeserializer.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package kafka.examples.common.serialization;
+
 
 import java.io.Serializable;
 import java.util.Map;
